1c39646d1e787f93941479dc243d74b4d903117a
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-14 17:40:40 +0530

    comparison operator

---
+++ comp.js
@@ -0,0 +1,9 @@
+//  console.log("2"==2);
+//  console.log("5"<1);
+ console.log(null==0);
+ console.log(null>0);
+ console.log(null>=0);
+ 
+//strict datatypes check (===)
+console.log("2"===2);
+console.log("2"==2);
