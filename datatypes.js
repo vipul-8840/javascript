60981dcc3f0d9c0008fd85fd56f14187209a181a
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-14 17:06:44 +0530

    tricky operations in js

--- datatypes.js
+++ datatypes.js
@@ -14,19 +14,32 @@
 // console.log(sum(10,15));
 
 //NaN=Not a Number property understanding 
-let score = "10";
-let val = Number(score);
-console.log(val);
+// let score = "10";
+// let val = Number(score);
+// console.log(val);
 
-let score1 = "abc";
-let val1 = Number(score1);
-console.log(val1);
+// let score1 = "abc";
+// let val1 = Number(score1);
+// console.log(val1);
 
-let score2 = false;
-let val2 = Number(score2);
-console.log(val2);
+// let score2 = false;
+// let val2 = Number(score2);
+// console.log(val2);
 
-let score3=null;
-let val3 = Number(score3);
-console.log(val3);
-console.log(typeof NaN);
+// let score3=null;
+// let val3 = Number(score3);
+// console.log(val3);
+// console.log(typeof NaN);
+
+// let a = 33;
+// let b = String(a);
+// console.log(b);
+// console.log(typeof b);
+// console.log("1"+"2");
+// console.log(1+"2");
+// console.log("1"-2);
+console.log(3-"1");
+console.log(1+5 +"6");
+console.log("6"+1+5);
+console.log(+true);
+console.log(+"");
