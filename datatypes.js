3f04e9654738da21c22b12020155f46db6947b1e
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-14 16:09:42 +0530

    datatype conversion

---
+++ datatypes.js
@@ -0,0 +1,32 @@
+// "use strict";
+// class bad{
+
+// }
+// let a = "vipul"
+// console.log(typeof a);
+// console.log(typeof null);
+// console.log(typeof bad);
+// function sum()
+// {
+//     console.log(typeof arguments);
+//     return arguments[0]+ arguments[1];
+// }
+// console.log(sum(10,15));
+
+//NaN=Not a Number property understanding 
+let score = "10";
+let val = Number(score);
+console.log(val);
+
+let score1 = "abc";
+let val1 = Number(score1);
+console.log(val1);
+
+let score2 = false;
+let val2 = Number(score2);
+console.log(val2);
+
+let score3=null;
+let val3 = Number(score3);
+console.log(val3);
+console.log(typeof NaN);
