f206be28e513e906fffb123ae495606ea017428f
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-15 17:30:41 +0530

    string and its methods

--- string.js
+++ string.js
@@ -18,3 +18,23 @@
 // console.log(obj.marks);
 // console.log(b.marks)
 
+// start learning to string 
+// console.log(typeof String);
+// const obj  = new String("vipul");
+// console.log(typeof obj);
+
+const obj = "vipul";
+// console.log(obj.length);
+// console.log(obj.substring(0,4));
+// console.log(obj.slice(-4,2));
+// console.log(obj.slice(-5,3));
+// console.log(obj.slice(-4,3));
+// console.log(obj.slice(-5,5));
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus);
+// const myBest = fruits.slice(-3, -1);
+// console.log(myBest);
+const url = "https://localhost:3000";
+// console.log(url.replace('s','r'));
+console.log(url.indexOf('s'));
