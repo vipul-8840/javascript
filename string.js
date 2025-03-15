5f03971c843d5dd532cfc55cb4f2fd02a7fb2cab
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-15 12:52:11 +0530

    stack & heap alloction of primitive & non primitive data types

---
+++ string.js
@@ -0,0 +1,20 @@
+// let user = 10;
+// let a  = user ;
+// a = 90;
+// console.log(user);
+// console.log(a);
+
+// let obj = {
+//     name:"Vipul",
+//     marks :70
+// }
+// let users = obj;
+// users.marks = 50;
+// console.log(users.marks);
+// console.log(obj.marks);
+// //spread opertor 
+// let b = {...obj};
+// b.marks=60;
+// console.log(obj.marks);
+// console.log(b.marks)
+
