79dc9211f8bcdaba419d9db3349cce79899b8ea4
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-15 19:32:51 +0530

    end

---
+++ number.js
@@ -0,0 +1,36 @@
+// const balance = new Number(1000);
+// console.log(typeof balance);
+
+// const balance = new Number(1000);
+// console.log(balance);
+// console.log(balance.toString().length);
+
+// const num = 23.890;
+// console.log(num.toPrecision(3));
+// const num1 = 123.89;
+// console.log(num1.toPrecision(3));
+// const num2 = 1123.89;
+// console.log(num2.toPrecision(3));
+
+// const a = Math.abs(-4);
+// console.log(a);
+            // ------------- Randonm() function --------
+
+            // max = 20;
+            // min = 10;
+            // const a =Math.floor(Math.random()*(max-min+1));
+            // console.log(a);
+            // console.log(a + min);
+            //  -----data in JS----
+        //    const date = new Date();
+        //    console.log(date);
+        //    const newDate = new Date(2025,3,15);
+        //    console.log(newDate.toDateString());
+        //    console.log(newDate.toLocaleString());
+        const date = new Date('2025-03-15');
+        console.log(Date.now());
+        console.log(Math.floor(Date.now()/1000));
+        console.log(date.getMonth() + 1);
+        console.log(date.getDay());
+        console.log(date.getFullYear());
+
