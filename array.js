648a39cb4a49fcb05b0f37cc554925032464127d
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-16 11:40:41 +0530

    learning array concept

---
+++ array.js
@@ -0,0 +1,27 @@
+
+// const arr = new Array(1,2,3,4);
+// console.log(arr);
+// arr.push(8);
+// arr.push(8);
+// arr.push(10);
+// arr.pop();
+// console.log(arr);
+
+// console.log(arr.length);
+// slice and splice method works different changes 
+
+// console.log(arr.slice(1,3));
+// console.log(arr);
+// console.log(arr.splice(1,3));
+// console.log(arr);
+// const arr = [1,2,3];
+// const arr1 = [4,5];
+// console.log(arr.push(arr1));
+// console.log(arr.pop());
+// console.log(arr);
+// const newArr = arr.concat(arr1);
+// console.log(newArr);
+// console.log(arr);
+
+// const newArr = [...arr,...arr1]
+// console.log(newArr);
