0a29c8d7348b74a44518e6f1944cb2d4965b66fb
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-16 17:43:25 +0530

    end

--- object.js
+++ object.js
@@ -37,3 +37,31 @@
 // console.log(human);
 // console.log(human['student']);
 // console.log(human.student.course);
+            // ------ Object.assign()-------
+// const target = { a: 1, b: 2 };
+// const source = { b: 4, c: 5 };
+// const returnedTarget = Object.assign(target, source);
+// console.log(target);
+// console.log(returnedTarget);
+// console.log(returnedTarget === target);
+
+// const obj1 ={1:"a",2:"b"};
+// const obj2 ={3:"c",4:"d"};
+// const obj3 = Object.assign(obj1,obj2);
+// console.log(obj3);
+// console.log(obj1);
+
+// const student ={
+//     id :'1',
+//     name:"vipul",
+
+// }
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
+const obj ={
+    name:"vipul",
+    age :22,
+    course :"maths"
+}
+const {name,age,course}=obj;
+console.log(name);
