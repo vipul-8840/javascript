ba489b9e6a5fd0d24e4690c2644350754c2091a4
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-16 17:04:38 +0530

    object properties

---
+++ object.js
@@ -0,0 +1,39 @@
+
+// const id = Symbol("1");
+// const obj =
+// {
+//     name:"vipul",
+//     "full_name":"kumar",
+//     age:"22",
+//     [id] :id
+// }
+// console.log(obj.name);
+// console.log(obj["age"]);
+// console.log(obj.fullname);
+// console.log(obj["full_name"]);
+// console.log(typeof obj.age )
+// console.log( obj[id])
+
+// const obj = new Object ()
+// console.log(obj);
+// const id = Symbol("1");
+// const student = {}
+// student[id]=Symbol("1");
+// student.name = "vipul";
+// student.age = 22;
+// student['fullName']= "Kumar";
+// console.log(student);
+
+// const human ={
+//     name :"vipul",
+//     student:{
+//         age:22,
+//         gender:"M",
+//         course:{
+//             subj :"Math"
+//         }
+//     }
+// }
+// console.log(human);
+// console.log(human['student']);
+// console.log(human.student.course);
