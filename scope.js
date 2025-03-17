a321cf9ab8bdaed39634f5f2bf7c02fe71bbde66
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-17 16:21:13 +0530

    end scope

---
+++ scope.js
@@ -0,0 +1,52 @@
+// let a = 20;
+// const c = 30;
+// var b = 10;
+// if(true)
+// {
+//     let a = 40;
+//     console.log(a);
+//     const c = 60;
+//     console.log(c);
+//     b = 20;
+//     console.log(b);
+// }
+// console.log(a);
+// console.log(c);
+// console.log(b);
+
+// if(true)
+// {
+//     const name = "vipul";
+//     if(name=="vipul")
+//     {
+//         const a = 5;
+//         console.log(name + a);
+//     }
+//     // console.log(a);
+
+// }
+// console.log(name);
+
+// function one()
+// {
+//     var a = 8;
+//     function two()
+//     {
+//         const name = "vipul";
+//         console.log(a);
+
+//     }
+//     // console.log(name);
+//     two();
+// }
+// one();
+console.log(One(5));
+function One(num)
+{
+    return num+1;
+}
+//   console.log(addTwo(5));
+const addTwo = function (num)
+{
+    return num + 1;
+}
