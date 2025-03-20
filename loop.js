a81f1ab3ef8b6eb4362881103ae841366549ed20
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-20 23:59:22 +0530

    for of loops

--- loop.js
+++ loop.js
@@ -14,8 +14,44 @@
 //     console.log(element);
     
 // }
-const greetings  = 'vipul';
-for(const greets of greetings)
-{
-    console.log(greets);
+// const greetings  = 'vipul';
+// for(const greets of greetings)
+// {
+//     console.log(greets);
+// }
+
+// maps
+const map = new Map();
+map.set('a',1);
+map.set('b',2);
+map.set('c',3);
+// console.log(map);
+
+// for(const key of map)
+// {
+//     console.log(key);
+// }
+// for(const [key,value] of map)
+// {
+//     console.log(key,':->',value);
+// }
+const obj ={
+    a :"1",
+    b:"2",
+    c:'3'
 }
+// for(const key in obj)
+// {
+//     console.log(key);
+// }
+for(const key in obj)
+{
+    console.log(obj[key]);
+    // console.log(key);
+}
+const arr = [1,2,3,4,5];
+for(const num of arr)
+{
+    console.log(num);
+}
+for(const )
