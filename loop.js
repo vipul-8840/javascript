bf896c7e1ac260a75a5f5a81fccebc6d85383488
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-20 23:26:24 +0530

    loops

---
+++ loop.js
@@ -0,0 +1,11 @@
+// let i = 0;
+// while(i<=10)
+// {
+//     console.log(`the value of the number is : ${i}`);
+//     i+=2;
+// }
+score = 1;
+do{
+     console.log(`score is ${score}`);
+     score++;
+}while(score<0)
