67888b8c4e1665bcad4a473ecaddef944844f321
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-20 23:36:59 +0530

    for of loop

--- loop.js
+++ loop.js
@@ -4,8 +4,18 @@
 //     console.log(`the value of the number is : ${i}`);
 //     i+=2;
 // }
-score = 1;
-do{
-     console.log(`score is ${score}`);
-     score++;
-}while(score<0)
+// score = 1;
+// do{
+//      console.log(`score is ${score}`);
+//      score++;
+// }while(score<0)
+// const arr = [1,2,3,4,5];
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+const greetings  = 'vipul';
+for(const greets of greetings)
+{
+    console.log(greets);
+}
