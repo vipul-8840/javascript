6348c2b032fbea8383d2e8a3277666fae1072fb1
vipul-8840 <vipulkumarlen@gmail.com>
2025-03-30 01:14:50 +0530

    parallel exection in async

--- async.js
+++ async.js
@@ -31,13 +31,38 @@ const promise = new Promise((resolve,reject)=>{
 //      console.log(pr);
 
 // }
-function printData()
-{
-   promise.then((res)=>{
-    console.log(res)
-   })
-   console.log("handled prmoise ")
-}
-printData();
+// function printData()
+// {
+//    promise.then((res)=>{
+//     console.log(res)
+//    })
+//    console.log("handled prmoise ")
+// }
+// printData();
 // print();
 // console.log("vipul");
+
+const pr1 = new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+        resolve("hello vipul")
+     },10000)
+});
+const pr2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve("hey aaina")
+    },5000 )
+});
+
+(async function handlePromise()
+{
+    let date = new Date();
+    console.log(date.toLocaleTimeString());
+
+    const val1 = await pr1;
+    console.log(val1);
+    console.log(new Date().toLocaleTimeString());
+    const val2 = await pr2;
+
+    console.log(val2)
+    console.log(new Date().toLocaleTimeString())
+})();
